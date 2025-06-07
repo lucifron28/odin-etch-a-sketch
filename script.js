292bd969efb366fb07d0782acfdb27ca65cd3cd7
ron <lucifron28@gmail.com>
2025-06-07 11:09:37 +0800

    fix the typo

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   const container = document.getElementsByClassName('grid-container')[0];
   const clearButton = document.getElementById('clear-button');
-  const sizeButton = document.getElementbyId('size-button');
+  const sizeButton = document.getElementById('size-button');
 
+  let size = 16;
 
+  function createGrid(size) {
+    container.style.gridTemplateColumns(`repeat(${size}, 1fr)`)
+    for (let i = 0; i < size * size; i++) {
+      const cell = document.createElement('div');
+      cell.classList.add('grid-cell');
+      container.appendChild(cell);
+    }
+
+  }
   sizeButton.addEventListener("click", () => {
-    let size = prompt("Enter the grid size: ");
+    size = prompt("Enter the grid size: ");
+    createGrid(size);
   });
 
-  for (let i = 0; i < size * size; i++) {
-    const cell = document.createElement('div');
-    cell.classList.add('grid-cell');
-    container.appendChild(cell);
-  }
+  createGrid(size);
 });
