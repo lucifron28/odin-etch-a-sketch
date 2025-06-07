8e942045a1276d2ea9272d243fa0ebbfce0e988f
ron <lucifron28@gmail.com>
2025-06-07 11:11:28 +0800

    add the dynamic sizing and grid templating

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
   function createGrid(size) {
     container.style.gridTemplateColumns(`repeat(${size}, 1fr)`)
+    container.style.gridTemplateRows(`repeat(${size}, 1fr)`)
     for (let i = 0; i < size * size; i++) {
       const cell = document.createElement('div');
       cell.classList.add('grid-cell');
