b1a62d911b7343e709bcf2b98ad9da29079216c7
ron <lucifron28@gmail.com>
2025-06-07 11:37:24 +0800

    fix the sketch

--- script.js
+++ script.js
@@ -6,17 +6,26 @@ document.addEventListener("DOMContentLoaded", (event) => {
   let size = 16;
 
   function createGrid(size) {
-    container.style.gridTemplateColumns(`repeat(${size}, 1fr)`)
-    container.style.gridTemplateRows(`repeat(${size}, 1fr)`)
+    container.innerHTML = ''
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     for (let i = 0; i < size * size; i++) {
       const cell = document.createElement('div');
-      cell.classList.add('grid-cell');
+      cell.classList.add('grid-cell'); 
+      cell.addEventListener('mouseover', () => {
+        cell.style.backgroundColor = 'black';
+      });
+
       container.appendChild(cell);
     }
 
   }
   sizeButton.addEventListener("click", () => {
     size = prompt("Enter the grid size: ");
+    createGrid(size);
+  });
+
+  clearButton.addEventListener("click", () => {
     createGrid(size);
   });
 
