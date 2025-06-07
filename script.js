7b80539575302ba4cd34a85b961923ac079d11f6
ron <lucifron28@gmail.com>
2025-06-07 10:58:26 +0800

    added prompt and remove hover for css

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   const container = document.getElementsByClassName('grid-container')[0];
-  
-  for (let i = 0; i < 16 * 16; i++) {
+  const clearButton = document.getElementById('clear-button');
+  const sizeButton = document.getElementbyId('size-button');
+
+
+  sizeButton.addEventListener("click", () => {
+    let size = prompt("Enter the grid size: ");
+  });
+
+  for (let i = 0; i < size * size; i++) {
     const cell = document.createElement('div');
     cell.classList.add('grid-cell');
     container.appendChild(cell);
